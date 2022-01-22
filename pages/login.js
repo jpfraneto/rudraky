4a0ffedbbfcbc35150287cc5a8b996bdf3231342
jpfraneto <jpfraneto@gmail.com>
2022-01-22 13:30:49 -0300

    Dia 1. Today the mission was to implement magics passwordless login logic, so that the users can have a great UX as they wont have to get new passwords up and running for the app. Im working with magic because it seems like an amazing service, and also because the course that Im following of nextjs uses it in its final app. I made good progress, and I feel good. The implementation of the login logic is important, and this is going to be the key component of the app so that I can move forward with the implementation of different functionalities in the future.

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,62 @@
+import styles from '../styles/Login.module.css';
+import Head from 'next/head';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { magic } from '../lib/magic-client';
+
 const Login = () => {
-  return <h1>Here goes the login form!</h1>;
+  const [email, setEmail] = useState('');
+  const [userMsg, setUserMsg] = useState('');
+  const router = useRouter();
+  const handleOnChangeEmail = e => {
+    setUserMsg('');
+    setEmail(e.target.value);
+  };
+  const handleLoginWithEmail = async e => {
+    e.preventDefault();
+    if (email.includes('@')) {
+      if (email === 'jpfraneto@gmail.com') {
+        try {
+          const didToken = await magic.auth.loginWithMagicLink({ email });
+          console.log('tje did token is: ', didToken);
+          if (didToken) {
+            router.push('/');
+          }
+        } catch (err) {
+          console.error('Something went wrong logging in', error);
+        }
+      } else {
+        setUserMsg('Ocurrió un error con ese email');
+      }
+    } else {
+      setUserMsg('Por favor ingresa un email válido!');
+    }
+  };
+  return (
+    <div className={styles.mainContainer}>
+      <Head>
+        <title>Login to RudraKY</title>
+      </Head>
+      <div className={styles.loginDiv}>
+        <div className={styles.wrapper}>
+          <div>
+            <h3>Inicia Sesión</h3>
+            <input
+              onChange={handleOnChangeEmail}
+              type='email'
+              placeholder='Correo Electrónico'
+              className={styles.emailInput}
+            />
+            <p className={styles.userMsg}>{userMsg}</p>
+          </div>
+
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Entrar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
